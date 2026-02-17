b3b5f75a1af4b7148533fd165024b2331f61a319
CDRY-Michael <razersmash096@gmail.com>
2026-02-17 21:07:32 +0545

    Coded MonthlyexpenseC

--- practical2/monthly_expenditureC.c
+++ practical2/monthly_expenditureC.c
@@ -7,9 +7,10 @@
 int main()
 {
     float foodExpense = 6000.0, leisureExpense = 2000.0, clothesExpense = 500.0;
+    float accomodation = 24000.0, travel = 2000.0;
     float totalSpent;
 
-    totalSpent = foodExpense + leisureExpense + clothesExpense;
+    totalSpent = foodExpense + leisureExpense + clothesExpense + accomodation + travel;
 
     printf("The total expenditure this month was Rs.%.2f\n\n",totalSpent);
 }
