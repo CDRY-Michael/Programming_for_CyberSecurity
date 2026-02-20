d04e103717311c54a526dd4040e49831a4d6b08c
CDRY-Michael <razersmash096@gmail.com>
2026-02-20 18:48:01 +0545

    Coded MonthlyexpenseD

---
+++ practical2/monthly_expenditureD.c
@@ -0,0 +1,31 @@
+// Prints Mothly Expenditure
+// Practical 2, Part 2 (d)
+// @author Michael
+
+#include <stdio.h>
+
+int main()
+{
+    float foodExpense, leisureExpense, clothesExpense, accomodation, travel;
+    float totalSpent;
+//Takes input    
+    printf("Enter Food Expenses: ");
+    scanf("%f", &foodExpense);
+
+    printf("Enter Leisure Expense: ");
+    scanf("%f", &leisureExpense);
+
+    printf("Enter Clothes Expense: ");
+    scanf("%f", &clothesExpense);
+
+    printf("Enter Accomodation: ");
+    scanf("%f", &accomodation);
+
+    printf("Enter travel: ");
+    scanf("%f", &travel);
+// Calcultes total expense
+    totalSpent = foodExpense + leisureExpense + clothesExpense + accomodation + travel;
+// Prints the Total Expense
+    printf("The total expenditure this month was Rs.%.2f\n\n",totalSpent);
+
+}
